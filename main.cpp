f2ece765128b4739c85234936fee8551121b7c7e
Timur <abdrakhimov07@list.ru>
2025-09-10 18:08:57 +0500

    it's third commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(void){
     std::cout << "hello world" << std::endl;
     std::cout << "second hello world" << std::endl;
     std::cout << "third hello world" << std::endl;
+    std::cout << R"I love C++ programm language" << std::endl;
 
 return 0;
 }
