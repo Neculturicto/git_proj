1f66f1f27c9d1b37b3f3d2c692f976ccd969c677
Timur <abdrakhimov07@list.ru>
2025-09-10 17:51:10 +0500

    added second commit

---
+++ cpp1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+template <typename T>
+void print(T data){
+	std::cout << data << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-int main(voud){
-    std::cout << "hello world\n";
-
+int main(void){
+    std::cout << "hello world" << std::endl;
+    std::cout << "second hello world" << std::endl;
+    std::cout << "third hello world" << std::endl;
 
 return 0;
 }
