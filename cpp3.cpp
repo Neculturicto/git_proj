71e91799f179aa1b261dca4de4135425e66eff73
Neculturicto <156899182+Neculturicto@users.noreply.github.com>
2025-09-10 18:45:33 +0500

    Update cpp3.cpp

--- cpp3.cpp
+++ cpp3.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 void f(void){
     std::cout << "it's void function" << std::endl;
-    std::cout << "hello form git" << std:endl;
+    std::cout << "hello form git" << std::endl;
 }
